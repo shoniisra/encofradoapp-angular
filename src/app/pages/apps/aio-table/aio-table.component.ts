bd0616ae04fdf8c02eb6bd9c89fb18f94724300d
shoniisrael <jivillacis4@espe.edu.ec>
2020-06-25 00:55:08 -0500

    Listar Clientes Funcional

--- src/app/pages/apps/aio-table/aio-table.component.ts
+++ src/app/pages/apps/aio-table/aio-table.component.ts
@@ -1,32 +1,42 @@
-import { AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { Observable, of, ReplaySubject } from 'rxjs';
-import { filter } from 'rxjs/operators';
-import { Cliente } from './interfaces/cliente.model';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatDialog } from '@angular/material/dialog';
-import { TableColumn } from '../../../../@vex/interfaces/table-column.interface';
-import { aioTableLabels } from '../../../../static-data/aio-table-data';
-import { CustomerCreateUpdateComponent } from './customer-create-update/customer-create-update.component';
-import icEdit from '@iconify/icons-ic/twotone-edit';
-import icDelete from '@iconify/icons-ic/twotone-delete';
-import icSearch from '@iconify/icons-ic/twotone-search';
-import icAdd from '@iconify/icons-ic/twotone-add';
-import icFilterList from '@iconify/icons-ic/twotone-filter-list';
-import { SelectionModel } from '@angular/cdk/collections';
-import icMoreHoriz from '@iconify/icons-ic/twotone-more-horiz';
-import icFolder from '@iconify/icons-ic/twotone-folder';
-import { fadeInUp400ms } from '../../../../@vex/animations/fade-in-up.animation';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions } from '@angular/material/form-field';
-import { stagger40ms } from '../../../../@vex/animations/stagger.animation';
-import { FormControl } from '@angular/forms';
-import { untilDestroyed } from 'ngx-take-until-destroy';
-import { MatSelectChange } from '@angular/material/select';
-import theme from '../../../../@vex/utils/tailwindcss';
-import icPhone from '@iconify/icons-ic/twotone-phone';
-import icMail from '@iconify/icons-ic/twotone-mail';
-import icMap from '@iconify/icons-ic/twotone-map';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { Observable, of, ReplaySubject } from "rxjs";
+import { filter } from "rxjs/operators";
+import { Cliente } from "./interfaces/cliente.model";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatDialog } from "@angular/material/dialog";
+import { TableColumn } from "../../../../@vex/interfaces/table-column.interface";
+import { aioTableLabels } from "../../../../static-data/aio-table-data";
+import { CustomerCreateUpdateComponent } from "./customer-create-update/customer-create-update.component";
+import icEdit from "@iconify/icons-ic/twotone-edit";
+import icDelete from "@iconify/icons-ic/twotone-delete";
+import icSearch from "@iconify/icons-ic/twotone-search";
+import icAdd from "@iconify/icons-ic/twotone-add";
+import icFilterList from "@iconify/icons-ic/twotone-filter-list";
+import { SelectionModel } from "@angular/cdk/collections";
+import icMoreHoriz from "@iconify/icons-ic/twotone-more-horiz";
+import icFolder from "@iconify/icons-ic/twotone-folder";
+import { fadeInUp400ms } from "../../../../@vex/animations/fade-in-up.animation";
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldDefaultOptions,
+} from "@angular/material/form-field";
+import { stagger40ms } from "../../../../@vex/animations/stagger.animation";
+import { FormControl } from "@angular/forms";
+import { untilDestroyed } from "ngx-take-until-destroy";
+import { MatSelectChange } from "@angular/material/select";
+import theme from "../../../../@vex/utils/tailwindcss";
+import icPhone from "@iconify/icons-ic/twotone-phone";
+import icMail from "@iconify/icons-ic/twotone-mail";
+import icMap from "@iconify/icons-ic/twotone-map";
 
 import { Apollo } from "apollo-angular";
 import gql from "graphql-tag";
@@ -51,28 +61,25 @@ const GetClientes = gql`
 `;
 
 @Component({
-  selector: 'vex-aio-table',
-  templateUrl: './aio-table.component.html',
-  styleUrls: ['./aio-table.component.scss'],
-  animations: [
-    fadeInUp400ms,
-    stagger40ms
-  ],
+  selector: "vex-aio-table",
+  templateUrl: "./aio-table.component.html",
+  styleUrls: ["./aio-table.component.scss"],
+  animations: [fadeInUp400ms, stagger40ms],
   providers: [
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: {
-        appearance: 'standard'
-      } as MatFormFieldDefaultOptions
-    }
-  ]
+        appearance: "standard",
+      } as MatFormFieldDefaultOptions,
+    },
+  ],
 })
 export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
   data: Observable<any>;
   loading: boolean;
   currentUser: any;
   error: any;
-  layoutCtrl = new FormControl('boxed');
+  layoutCtrl = new FormControl("boxed");
 
   /**
    * Simulating a service with HTTP that returns Observables
@@ -81,22 +88,38 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
   subject$: ReplaySubject<Cliente[]> = new ReplaySubject<Cliente[]>(1);
   data$: Observable<Cliente[]> = this.subject$.asObservable();
   customers: Cliente[];
+  // customers: Observable<any>;
 
   @Input()
   columns: TableColumn<Cliente>[] = [
-    { label: 'Checkbox', property: 'checkbox', type: 'checkbox', visible: true },
-    { label: 'cedula' , property: 'cedula' , type: 'text', visible:true },
-    { label: 'direccion' , property: 'direccion' , type: 'text', visible:true },
-    { label: 'direccion2' , property: 'direccion2' , type: 'text', visible:true },
-    { label: 'email' , property: 'email' , type: 'text', visible:true },
-    { label: 'nombre' , property: 'nombre' , type: 'text', visible:true },
-    { label: 'nombre2' , property: 'nombre2' , type: 'text', visible:true },
-    { label: 'observacion' , property: 'observacion' , type: 'text', visible:true },
-    { label: 'telf1' , property: 'telf1' , type: 'text', visible:true },
-    { label: 'telf2' , property: 'telf2' , type: 'text', visible:true },
-    { label: 'telf3' , property: 'telf3' , type: 'text', visible:true },      
-    { label: 'Labels', property: 'labels', type: 'button', visible: true },
-    { label: 'Actions', property: 'actions', type: 'button', visible: true }
+    {
+      label: "Checkbox",
+      property: "checkbox",
+      type: "checkbox",
+      visible: true,
+    },
+    { label: "cedula", property: "cedula", type: "text", visible: true },
+    { label: "direccion", property: "direccion", type: "text", visible: true },
+    {
+      label: "direccion2",
+      property: "direccion2",
+      type: "text",
+      visible: true,
+    },
+    { label: "email", property: "email", type: "text", visible: true },
+    { label: "nombre", property: "nombre", type: "text", visible: true },
+    { label: "nombre2", property: "nombre2", type: "text", visible: true },
+    {
+      label: "observacion",
+      property: "observacion",
+      type: "text",
+      visible: true,
+    },
+    { label: "telf1", property: "telf1", type: "text", visible: true },
+    { label: "telf2", property: "telf2", type: "text", visible: true },
+    { label: "telf3", property: "telf3", type: "text", visible: true },
+    { label: "Labels", property: "labels", type: "button", visible: true },
+    { label: "Actions", property: "actions", type: "button", visible: true },
   ];
   pageSize = 10;
   pageSizeOptions: number[] = [5, 10, 20, 50];
@@ -122,37 +145,37 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
-  constructor(private dialog: MatDialog, private apollo: Apollo) {
-  }
+  constructor(private dialog: MatDialog, private apollo: Apollo) {}
 
   get visibleColumns() {
-    return this.columns.filter(column => column.visible).map(column => column.property);
+    return this.columns
+      .filter((column) => column.visible)
+      .map((column) => column.property);
   }
 
-  /**
-   * Example on how to get data and pass it to the table - usually you would want a dedicated service with a HTTP request for this
-   * We are simulating this request here.
-   */
- 
-  ngOnInit() {
+  getData() {
     this.data = this.apollo
-    .watchQuery({ query: GetClientes })
-    .valueChanges.pipe(map(({ data }) => data.cliente));
+      .watchQuery({ query: GetClientes })
+      .valueChanges.pipe(map(({ data }) => data.cliente));
 
-   
+    return this.data;
+  }
+
+  ngOnInit() {
+    this.getData().subscribe((customers) => {
+      this.subject$.next(customers);
+    });
 
     this.dataSource = new MatTableDataSource();
 
-    this.data$.pipe(
-      filter<Cliente[]>(Boolean)
-    ).subscribe(customers => {
+    this.data$.pipe(filter<Cliente[]>(Boolean)).subscribe((customers) => {
       this.customers = customers;
       this.dataSource.data = customers;
     });
 
-    this.searchCtrl.valueChanges.pipe(
-      untilDestroyed(this)
-    ).subscribe(value => this.onFilterChange(value));
+    this.searchCtrl.valueChanges
+      .pipe(untilDestroyed(this))
+      .subscribe((value) => this.onFilterChange(value));
   }
 
   ngAfterViewInit() {
@@ -161,38 +184,46 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   createCustomer() {
-    this.dialog.open(CustomerCreateUpdateComponent).afterClosed().subscribe((customer: Cliente) => {
-      /**
-       * Customer is the updated customer (if the user pressed Save - otherwise it's null)
-       */
-      if (customer) {
+    this.dialog
+      .open(CustomerCreateUpdateComponent)
+      .afterClosed()
+      .subscribe((customer: Cliente) => {
         /**
-         * Here we are updating our local array.
-         * You would probably make an HTTP request here.
+         * Customer is the updated customer (if the user pressed Save - otherwise it's null)
          */
-        this.customers.unshift(new Cliente(customer));
-        this.subject$.next(this.customers);
-      }
-    });
+        if (customer) {
+          /**
+           * Here we are updating our local array.
+           * You would probably make an HTTP request here.
+           */
+          this.customers.unshift(new Cliente(customer));
+          this.subject$.next(this.customers);
+        }
+      });
   }
 
   updateCustomer(customer: Cliente) {
-    this.dialog.open(CustomerCreateUpdateComponent, {
-      data: customer
-    }).afterClosed().subscribe(updatedCustomer => {
-      /**
-       * Customer is the updated customer (if the user pressed Save - otherwise it's null)
-       */
-      if (updatedCustomer) {
+    this.dialog
+      .open(CustomerCreateUpdateComponent, {
+        data: customer,
+      })
+      .afterClosed()
+      .subscribe((updatedCustomer) => {
         /**
-         * Here we are updating our local array.
-         * You would probably make an HTTP request here.
+         * Customer is the updated customer (if the user pressed Save - otherwise it's null)
          */
-        const index = this.customers.findIndex((existingCustomer) => existingCustomer.id === updatedCustomer.id);
-        this.customers[index] = new Cliente(updatedCustomer);
-        this.subject$.next(this.customers);
-      }
-    });
+        if (updatedCustomer) {
+          /**
+           * Here we are updating our local array.
+           * You would probably make an HTTP request here.
+           */
+          const index = this.customers.findIndex(
+            (existingCustomer) => existingCustomer.id === updatedCustomer.id
+          );
+          this.customers[index] = new Cliente(updatedCustomer);
+          this.subject$.next(this.customers);
+        }
+      });
   }
 
   deleteCustomer(customer: Cliente) {
@@ -200,7 +231,12 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
      * Here we are updating our local array.
      * You would probably make an HTTP request here.
      */
-    this.customers.splice(this.customers.findIndex((existingCustomer) => existingCustomer.id === customer.id), 1);
+    this.customers.splice(
+      this.customers.findIndex(
+        (existingCustomer) => existingCustomer.id === customer.id
+      ),
+      1
+    );
     this.selection.deselect(customer);
     this.subject$.next(this.customers);
   }
@@ -210,7 +246,7 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
      * Here we are updating our local array.
      * You would probably make an HTTP request here.
      */
-    customers.forEach(c => this.deleteCustomer(c));
+    customers.forEach((c) => this.deleteCustomer(c));
   }
 
   onFilterChange(value: string) {
@@ -237,9 +273,9 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+    this.isAllSelected()
+      ? this.selection.clear()
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
   }
 
   trackByProperty<T>(index: number, column: TableColumn<T>) {
@@ -247,11 +283,10 @@ export class AioTableComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   onLabelChange(change: MatSelectChange, row: Cliente) {
-    const index = this.customers.findIndex(c => c === row);
+    const index = this.customers.findIndex((c) => c === row);
     this.customers[index].labels = change.value;
     this.subject$.next(this.customers);
   }
 
-  ngOnDestroy() {
-  }
+  ngOnDestroy() {}
 }
