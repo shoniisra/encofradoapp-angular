19714cf2d1faa851860123a205c97e8e832fc527
shoniisrael <jivillacis4@espe.edu.ec>
2020-07-08 02:29:03 -0500

    recibe objeto contrato alquiler en mutation

--- src/app/pages/apps/contratos/graphql/CreateContratoGQL.ts
+++ src/app/pages/apps/contratos/graphql/CreateContratoGQL.ts
@@ -7,10 +7,27 @@ import gql from "graphql-tag";
 })
 export class CreateContratoGQL extends Mutation {
   document = gql`
-    mutation MyMutation(   $area: jsonb!    
+    mutation MyMutation(
+      $numero: String!
+      $fecha: Date!
+      $lugar_obra: String!
+      $area: String!
+      $metros: String!
+      $observacion: String!
+      $estado_id: number!
+      $cliente_id: number!
     ) {
       insert_contrato_alquiler_one(
-        object: $area
+        object: {
+          area: "28"
+          estado_id: 2
+          fecha: "2020-07-05"
+          lugar_obra: "mi casa"
+          metros: "26"
+          observacion: "ninguna"
+          numero: 5
+          cliente_id: 12
+        }
       ) {
         id
       }
